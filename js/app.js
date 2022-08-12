6ccff1eb2df7ff9c77955aadf4e015adb6829f2d
zizo-bond <108358567+zizo-bond@users.noreply.github.com>
2022-08-13 00:38:08 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,97 +1,113 @@
            
            /**
- *
+ * 
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- *
+ * 
  * Dependencies: None
- *
+ * 
  * JS Version: ES2015/ES6
- *
+ * 
  * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+ * 
  */
 
 /**
  * Define Global Variables
- *
+ * 
  */
- const navBarList = document.getElementById("navbar__list"); // empty list
-const navBarMenuSections = [...document.querySelectorAll("section")]; // makes an array and add items to it for each section in the document
-let navBarMenuItems = navBarMenuSections.length;
-const listSections = document.querySelectorAll("section"); // section elements
-const listlinks = document.querySelectorAll(".navbar__menu a"); // link in the navbar
+const sections = document.getElementsByTagName('section');
+const list = document.getElementById('navbar__list');
+const list_array = document.getElementById('navbar__list').childNodes; // consider list items as an array
+const header = document.querySelector('.page__header');
+
+
+//  Define sections' pageYOffset values 
+const sectionsOffsetVAL = [];
+let activeSection;
+let activeSection_ids;
 /**
  * End Global Variables
+ 
  * Begin Main Functions
- *
+ * 
  */
 
-// build the nav
-
-const myNavBar = function AddItems() {
-    // function to add items to the nav-bar list
-    for (navBarMenuSection of navBarMenuSections) {
-        navBarMenuSectionName = navBarMenuSection.getAttribute("data-nav");
-        navBarMenuSectionLink = navBarMenuSection.getAttribute("id");
-        navBarMenuListItem = document.createElement("li");
-        navBarMenuListItem.innerHTML = `<a class='menu__link' href='#${navBarMenuSectionLink}'>${navBarMenuSectionName}</a>`;
-        navBarList.appendChild(navBarMenuListItem);
+// making the nav
+const myNav = function()  {
+    for (let section of sections) {
+        let li = section.attributes['data-nav'].nodeValue;
+        list.innerHTML += `<li><a href="" class="menu__link">${li}</a></li>`;
+        // get the sections' pageYOffset values 
+        sectionsOffsetVAL.push(section.offsetTop);
     }
-};
+    // making the first 'li' item as active
+    list.childNodes[0].classList.add('active-nav');
+}
+// calling the function
+myNav();
 
-// Add class 'active' to section when near top of viewport
-
-const sectionInViewport = function sectionPos(view) {
-    // determine if section is near top of viewport
-    let sectionz = view.getBoundingClientRect();
-    return sectionz.top <= 200 && sectionz.bottom >= 200;
-};
-
-const addActiveClass = function acitveClass() {
-    // function to add active class to viewed section
-    for (navBarMenuSection of navBarMenuSections) {
-        if (sectionInViewport(navBarMenuSection)) {
-            if (!navBarMenuSection.classList.contains("active-class")) {
-                navBarMenuSection.classList.add("active-class");
-            }
-        } else {
-            navBarMenuSection.classList.remove("active-class");
-        }
-    }
-};
-
-// Scroll smoothly to section on anchor click
-
-const smoothScroll = function scrollSmooth() {
-    document.querySelectorAll(".menu__link").forEach((anchor) => {
-        // selects all anchors with class='menu__link'
-        anchor.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.querySelector(anchor.getAttribute("href")).scrollIntoView({
-                behavior: "smooth",
-            });
-        });
-    });
-};
 /**
  * End Main Functions
  * Begin Events
- *
+ * 
  */
 
-// Build menu
+// Detecting the active section through tracking the window YOffset 
 
-myNavBar();
+window.addEventListener('scroll', (e) => {
 
-// Scroll to section on link click
+    // check if the first section has been reached
+    if (sectionsOffsetVAL && window.pageYOffset >= sectionsOffsetVAL[0]) {
+        //  detect the active section  by making a loop 
+        sectionsOffsetVAL.map((value, index) => {
+            // setting offset value to 200 to avoid changing active class in case of  hitting the last few px of the section
+            if (value <= window.pageYOffset + 200) {
+                // define the active section to add the active class to it
+                activeSection = sections[index];
+                activeSection_ids = index;
+            }
+        })
 
-smoothScroll();
+        // check if the active class is already in the active section's classList
+        if (activeSection.classList.contains("your-active-class")) {
+            return
+        } else {
+            // remove the  active class from all sections 
+            for (let section of sections) {
+                section.classList.remove("your-active-class")
+            }
+            // remove the active class from all nav li 
+            for (let li of list_array) {
+                li.classList.remove("active-nav")
+            }
+            // add the active class only to the active section
+            activeSection.classList.add("your-active-class") 
+            // add the active class  only to the active nav li
+            list_array[activeSection_ids].classList.add("active-nav")
+        }
+    }
+})
 
+
+
+
+// Scroll to section on click
+list.addEventListener('click', (e) => {
+    // prevent a(href) default behaviour
+    e.preventDefault();
+    console.dir(e.target.nodeName)
+    if (e.target.nodeName == 'A') {
+        for (let section of sections) {
+            if (e.target.outerText == section.attributes['data-nav'].nodeValue) {
+                window.scrollTo({
+                    top: section.offsetTop,
+                    left: section.offsetLeft,
+                    behavior: 'smooth'
+                })
+            }
+        }
+    }
+})
